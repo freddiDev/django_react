5f6ae729e33be74c4ae0262c5a733e19bdbf10a4
erp_dev_001 <fredditampubolon66@gmail.com>
2024-09-15 15:52:37 +0700

    custom user model

--- core/settings/base.py
+++ core/settings/base.py
@@ -137,7 +137,7 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# AUTH_USER_MODEL = "users.User"
+AUTH_USER_MODEL = "users.User"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
