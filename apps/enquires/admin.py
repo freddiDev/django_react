389b93032a3640cc16e0c6e5dd0f3e3d63688ae9
erp_dev_001 <fredditampubolon66@gmail.com>
2024-09-16 14:11:58 +0700

    add property queries and serializers

--- apps/enquires/admin.py
+++ apps/enquires/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Enquiry
+
+
+class EnquiryAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "phone", "message"]
+
+
+admin.site.register(Enquiry, EnquiryAdmin)
